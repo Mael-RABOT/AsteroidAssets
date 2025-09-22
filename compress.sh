73f27fa48b2b7897f195afc2e1446859fa97bd29
Mael-RABOT <mael.rabot@gmail.com>
2025-09-22 16:59:49 +0900

    evol(script): remove compression script
    Signed-off-by: Mael-RABOT <mael.rabot@gmail.com>

--- compress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SRC_DIR="${1:-.}"
-ARCHIVE_NAME="assets_archive.tar.gz"
-
-find "$SRC_DIR" -type f \( -iname "*.glb" -o -iname "*.hdr" -o -iname "*.png" \) \
-  -print0 | tar --null -czvf "$ARCHIVE_NAME" --files-from=-
-
